31fbc4c3fe7f6dc70f25e1d72cab4183d9ec2013
harrry <xxxxxx@qq.com>
2025-09-23 19:02:34 +0800

    init: create servlet

--- adminUI/src/main/java/com/atlassian/plugins/tutorial/refapp/impl/MyPluginComponentImpl.java
+++ adminUI/src/main/java/com/atlassian/plugins/tutorial/refapp/impl/MyPluginComponentImpl.java
@@ -1,14 +1,13 @@
 package com.atlassian.plugins.tutorial.refapp.impl;
 
-import com.atlassian.sal.api.ApplicationProperties;
 import com.atlassian.plugins.tutorial.refapp.api.MyPluginComponent;
+import com.atlassian.sal.api.ApplicationProperties;
 
 
-public class MyPluginComponentImpl implements MyPluginComponent
-{
-        private final ApplicationProperties applicationProperties;
+public class MyPluginComponentImpl implements MyPluginComponent {
+    private final ApplicationProperties applicationProperties;
 
-    
+
     public MyPluginComponentImpl(final ApplicationProperties applicationProperties) {
         this.applicationProperties = applicationProperties;
     }
